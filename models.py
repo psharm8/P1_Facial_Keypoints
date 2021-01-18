8eee1ed0f5bffeb4ea699cb7d86135fa835f42aa
Puneet Sharma <puneet.s2004@gmail.com>
2021-01-18 13:19:50 -0500

    mostly done

--- models.py
+++ models.py
@@ -21,17 +21,69 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        self.bnorm1 = nn.BatchNorm2d(32)
+        # Input image size if 224x224 as per the crop defined in data prep
+        # output size after conv1: (224 - 5)/1 + 1 = 220
+        self.drop1 = nn.Dropout(p=0.1)
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.pool = nn.MaxPool2d(2, 2)
+        # output size after pool: 222/2 = 110
+
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.bnorm2 = nn.BatchNorm2d(64)
+        # output size after conv2: (110-3)/1 + 1 = 108
+        # after pool 108/2 = 54
+        self.drop2 = nn.Dropout(p=0.2)
+
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.bnorm3 = nn.BatchNorm2d(128)
+        # output size after conv3: (54-3)/1 + 1 = 52
+        # after pool 52/2 = 26
+        self.drop3 = nn.Dropout(p=0.3)
+
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.bnorm4 = nn.BatchNorm2d(256)
+        # output size after conv4: (26-3)/1 + 1 = 24
+        # after pool 24/2 = 12
+        self.drop4 = nn.Dropout(p=0.4)
+
+        self.fc1 = nn.Linear(256*12*12, 1000)
+        self.fc1_drop = nn.Dropout(p=0.5)
+
+
+        self.fc2 = nn.Linear(1000, 1000)
+        self.fc2_drop = nn.Dropout(p=0.6)
+
+        self.fc3 = nn.Linear(1000, 136)
         
 
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = (self.pool(F.relu(self.conv1(x))))        
+        x = self.drop1(x)
+
+        x = (self.pool(F.relu(self.conv2(x))))        
+        x = self.drop2(x)
+
+        x = (self.pool(F.relu(self.conv3(x))))        
+        x = self.drop2(x)
+
+        x = (self.pool(F.relu(self.conv4(x))))
+        x = self.drop3(x)
+
+        x = x.view(x.size(0), -1)
         
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
         
+        x = F.relu(self.fc2(x))
+        x = self.fc2_drop(x)
+        
+        x = self.fc3(x)
+                
         # a modified x, having gone through all the layers of your model, should be returned
         return x
